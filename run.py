a18ff3f75e07f263078d4c32fb146b549f9ff922
Andrei Aldea <Grippentech@users.noreply.github.com>
2018-04-27 14:04:26 -0400

    Added trial SMU setup

--- run.py
+++ run.py
@@ -22,9 +22,19 @@ print("\nConnected to Instrument: " + my_instrument.query('*IDN?') + "\n")
 active = True
 subActive = True
 
+#The following is to make input work in python 2
+##try:
+##    input = raw_input
+##except NameError:
+##    pass
+
+def getChan(): #Displays Channel Data
+	my_instrument.write(":PAGE:CHAN")
+	print(my_instrument.query("HCOPy:DATA?"))
+
 while active:
 	print("What would you like to do: \n")
-	print("1 - Start New Measurement")
+	print("1 - Setup Measurement")
 	print("2 - Change Device Settings")
 	print("3 - Get Data")
 	print("4 - Force GPIB/HPIB Query")
@@ -32,6 +42,21 @@ while active:
 
 	whatDo = input(consoleSeq)
 	if whatDo == "1":
+		subActive = True
+		print("\n Current Setup: \n")
+		getChan();
+		while subActive:
+			print("You Can: (Sample Command: 1:SMU1:IPULSE)\n")
+			print("1 - Set Measurement Mode  (V/I/VPUL(se)/IPUL(se)/COMM(on)/DIS(able))")
+			whatDoSub = input(consoleSeq)
+			if whatDoSub[0] == "1":
+				my_instrument.query("PAGE:CHAN:" + whatDoSub.split(":")[1] + whatDoSub.split(":")[2])
+				pass
+			elif whatDoSub == "exit":
+				subActive = False
+			else:
+				print("\nWrong Input, Try Again\n")
+				pass
 		pass
 	elif whatDo == "2":
 		pass
@@ -41,10 +66,14 @@ while active:
 	elif whatDo == "4":
 		print("\nEnter GPIB Queries, or type \"exit\"...\n")
 		while(subActive == True):
-			user_input = input(consoleSeq).lower()
-			if(user_input != "exit"):
+			user_input = input(consoleSeq)
+			if(user_input.lower() != "exit"):
 				subActive = True
-				print(my_instrument.query(user_input))
+				my_instrument.write(user_input)
+				try:
+					print(my_instrument.read())
+				except:
+					pass
 			else:
 				subActive = False
 		pass
