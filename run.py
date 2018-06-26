43cdb171e5e2e88ada9ad42586eaf1f1b780ac4e
grippy98 <grippy98@github.com>
2018-06-26 15:17:57 +0100

    Testing Mode setting - At least 2 big bugs remain

--- run.py
+++ run.py
@@ -217,6 +217,8 @@ def writeLabels():
 	VMU1.vname = VMU1.vname_label.get()
 	VMU2.vname = VMU2.vname_label.get()
 
+	SMU1.mode = SMU1.mode_thing.get()
+
 	SMU1.writeParams()
 	SMU1.writeExtendedParams()
 	SMU2.writeParams()
